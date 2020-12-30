147ca59bbe99e54ed84dc3e2715e7eb7293b558e
fxnitsch <stelar@gmx.at>
2020-12-30 23:30:06 +0100

    fixed bug for duplicate values in code; extracting functions; refactoring

--- mastermind.py
+++ mastermind.py
@@ -24,16 +24,37 @@ def get_guess(digits):
     return guess
 
 
-def compare_input(guess, code, flags):
+def compare_input(guess, code):
     """ Checks user guess and returns flags """
-    for position, i in enumerate(code):
-        if guess[position] == i:
-            flags[position] = "X"
-        elif i in guess:
-            flags[position] = "0"
-        else:
-            flags[position] = "-"
+    flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
+    flags = check_for_content_matches(code_to_check, flags, guess_to_check)
+    while len(flags) != len(code):
+        flags.append("-")
     return sorted(flags)
+
+
+def check_for_content_matches(code, flags, guess):
+    """ Checks for content matches of `code` in `guess` and returns `flags` """
+    for position, digit in enumerate(code):
+        if digit in guess:
+            flags.append("0")
+            guess.remove(digit)
+    return flags
+
+
+def check_exact_matches(code, guess):
+    """ Checks `guess` for exact matches in `code` and returns 'X' in `flags` """
+    guess_to_check = guess.copy()
+    code_to_check = code.copy()
+    flags = []
+    for position, digit in enumerate(code):
+        if guess[position] == digit:
+            flags.append("X")
+            guess_to_check[position] = 'checked'
+            code_to_check[position] = 'checked'
+    guess_to_check = [value for value in guess_to_check if value != 'checked']
+    code_to_check = [value for value in code_to_check if value != 'checked']
+    return flags, code_to_check, guess_to_check
 
 
 def check_for_win(flags):
@@ -49,17 +70,16 @@ def run():
     chances = 6
     digits = 4
     code = create_code(digits)
-    flags = ['-'] * digits
     print("Guess the {} digit code with values between 1 and 6. "
           "You may only use a maximum of {} guesses. \n".format(digits, chances))
     while turn <= chances:
         guess = get_guess(digits)
-        flags = compare_input(guess, code, flags)
+        flags = compare_input(guess, code)
         if check_for_win(flags):
             print("You solved it in {} turns! Congratulations, master mind.".format(turn))
             quit()
         else:
-            print(" Turn {}:".format(turn), flags, "\n")
+            print(" Turn {}/{}:".format(turn, chances), flags, "\n")
             turn += 1
     print("You ran out of guesses. \n The code is {}".format(code))
 
