2f474afd87ea0dc4893e7c40c54b0136d30abe54
fxnitsch <stelar@gmx.at>
2021-01-01 14:44:46 +0100

    implemented test_check_for_win(); minortest refactoring

--- mastermind_tests.py
+++ mastermind_tests.py
@@ -1,58 +1,62 @@
 import unittest
 
-from mastermind import check_exact_matches, check_for_content_matches
+from mastermind import check_exact_matches, check_for_content_matches, check_for_win
 
 
 class MyTestCase(unittest.TestCase):
-    @staticmethod
-    def test_check_exact_matches():
+    def test_check_exact_matches(self):
         code = [1, 3, 3, 5]
 
         # some true
         guess = [1, 2, 3, 4]
         expected = [2, 4]
         flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
-        assert flags == ['X', 'X']
-        assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
-        assert expected == guess_to_check
+        self.assertEqual(flags, ['X', 'X'])
+        self.assertEqual(expected, guess_to_check)
 
         # all true
         guess = [1, 3, 3, 5]
         expected = []
         flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
-        assert flags == ['X', 'X', 'X', 'X']
-        assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
-        assert expected == guess_to_check
+        self.assertEqual(flags, ['X', 'X', 'X', 'X'])
+        self.assertEqual(expected, guess_to_check)
 
         # all false
         guess = [2, 2, 2, 2]
         expected = [2, 2, 2, 2]
         flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
         assert flags == []
-        assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
-        assert expected == guess_to_check
+        self.assertEqual(expected, guess_to_check)
 
-    @staticmethod
-    def test_check_for_content_matches():
+    def test_check_for_content_matches(self):
         code = [1, 3, 3, 5]
 
         # some true
         guess = [3, 2, 5, 3]
         flags = []
         flags = check_for_content_matches(code, flags, guess)
-        assert flags == ['0', '0', '0']
+        self.assertEqual(flags, ['0', '0', '0'])
 
         # all true
         guess = [3, 1, 5, 3]
         flags = []
         flags = check_for_content_matches(code, flags, guess)
-        assert flags == ['0', '0', '0', '0']
+        self.assertEqual(flags, ['0', '0', '0', '0'])
 
         # all false
         guess = [4, 4, 4, 4]
         flags = []
         flags = check_for_content_matches(code, flags, guess)
-        assert flags == []
+        self.assertEqual(flags, [])
+
+    def test_check_for_win(self):
+        # win
+        flags = ['X', 'X', 'X', 'X']
+        self.assertTrue(check_for_win(flags))
+
+        # no win
+        flags = ['X', 'X', 'X', '0']
+        self.assertFalse(check_for_win(flags))
 
 
 if __name__ == '__main__':
