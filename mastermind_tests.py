1e2298682716c992ab0d46b1e94363b54c659119
fxnitsch <stelar@gmx.at>
2021-01-01 14:26:23 +0100

    implemented test for check_content_matches()

--- mastermind_tests.py
+++ mastermind_tests.py
@@ -1,33 +1,58 @@
 import unittest
 
-from mastermind import check_exact_matches
+from mastermind import check_exact_matches, check_for_content_matches
 
 
 class MyTestCase(unittest.TestCase):
-    def test_check_exact_matches(self):
-        # some true
+    @staticmethod
+    def test_check_exact_matches():
         code = [1, 3, 3, 5]
+
+        # some true
         guess = [1, 2, 3, 4]
         expected = [2, 4]
-        _, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        assert flags == ['X', 'X']
         assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
         assert expected == guess_to_check
 
         # all true
-        code = [1, 3, 3, 5]
         guess = [1, 3, 3, 5]
         expected = []
-        _, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        assert flags == ['X', 'X', 'X', 'X']
         assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
         assert expected == guess_to_check
 
         # all false
-        code = [1, 3, 3, 5]
         guess = [2, 2, 2, 2]
         expected = [2, 2, 2, 2]
-        _, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        flags, code_to_check, guess_to_check = check_exact_matches(code, guess)
+        assert flags == []
         assert len(code_to_check) == len(expected) & len(guess_to_check) == len(expected)
         assert expected == guess_to_check
+
+    @staticmethod
+    def test_check_for_content_matches():
+        code = [1, 3, 3, 5]
+
+        # some true
+        guess = [3, 2, 5, 3]
+        flags = []
+        flags = check_for_content_matches(code, flags, guess)
+        assert flags == ['0', '0', '0']
+
+        # all true
+        guess = [3, 1, 5, 3]
+        flags = []
+        flags = check_for_content_matches(code, flags, guess)
+        assert flags == ['0', '0', '0', '0']
+
+        # all false
+        guess = [4, 4, 4, 4]
+        flags = []
+        flags = check_for_content_matches(code, flags, guess)
+        assert flags == []
 
 
 if __name__ == '__main__':
